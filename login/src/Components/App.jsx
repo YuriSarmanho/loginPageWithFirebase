6db61d0e455eda94fd38efddd5d380f17aa3b75c
YuriSarmanho <yuri.sarmanho@hotmail.com>
2021-11-14 23:11:27 -0300

    base da parte de login

---
+++ login/src/Components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SingUp from './SingUp'
+
+function App() {
+  return (
+    <SingUp/>
+  );
+}
+
+export default App;
---
+++ login/src/Components/SingUp.jsx
@@ -0,0 +1,41 @@
+import React,{useRef} from 'react';
+import { Form ,Button ,Card } from 'react-bootstrap'
+
+export default function SingUp() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
+
+    return (
+       <>
+        <Card>
+            <Card.Body>
+                <h2 className='text-center mb-4'>Sing Up</h2>
+                <Form>
+                    <Form.Group id='email'>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type='email' ref={emailRef} required/>
+                    </Form.Group>
+                    <Form.Group id='password'>
+                        <Form.Label>PassWord</Form.Label>
+                        <Form.Control type='password' ref={passwordRef} required/>
+                    </Form.Group>
+                    <Form.Group id='password-confirm'>
+                        <Form.Label>PassWord Cofirmation</Form.Label>
+                        <Form.Control type='password' ref={passwordConfirmRef} required/>
+                    </Form.Group>
+                    <Button className='w-100' type='submit'>Sing Up</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+
+
+        <div className= 'w-100 text-center mt-2'>
+            Already have  an account? Log In 
+        </div>
+       </>
+    );
+}
+
+export default Components;
