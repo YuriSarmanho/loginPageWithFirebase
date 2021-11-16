15ca060df96ea63c15a94107e7fdbeb2974635fb
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2021-11-16 17:53:20 -0300

    add context e visual do pagina de login

--- login/src/Components/App.jsx
+++ login/src/Components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import SingUp from './SingUp'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <SingUp/>
+    <Container Classname='d-flex aling-items-center justity-content-center'
+    style={{ minHeight: "100vh" }}
+    >
+    <div className='w-100' style={{ maxWidth: '400px' }}>
+      <SingUp/>
+    </div>
+    </Container>
   );
 }
+
 
 export default App;
--- login/src/index.jsx
+++ login/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />   
   </React.StrictMode>,
   document.getElementById('root')
 );
